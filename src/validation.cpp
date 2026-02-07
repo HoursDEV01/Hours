c9aea7592dec849bd8e73929b39de0961469b154
HoursDEV01 <charleswbloss@gmail.com>
2026-02-07 16:25:54 -0500

    Update validation.cpp
    Update to subsidy function

--- src/validation.cpp
+++ src/validation.cpp
@@ -1266,12 +1266,12 @@ bool ReadRawBlockFromDisk(std::vector<uint8_t>& block, const CBlockIndex* pindex
 CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
 {
     int halvings = nHeight / consensusParams.nSubsidyHalvingInterval;
-    // Force block reward to zero when right shift is undefined.
-    if (halvings >= 64)
-        return 0;
+    // Force block reward to 2 when right shift is undefined.
+    if (halvings >= 4)
+        return 2;
 
-    CAmount nSubsidy = 50 * COIN;
-    // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
+    CAmount nSubsidy = 16 * COIN;
+    // Subsidy is cut in half every 840,000 blocks 
     nSubsidy >>= halvings;
     return nSubsidy;
 }
